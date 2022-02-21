6d4c0aa97a4646220d48999ae0cd06a47afa43bc
hersapad <98178539+hersapad@users.noreply.github.com>
2022-02-21 21:06:41 +0300

    Update heart.py

--- RenameBot/heart.py
+++ RenameBot/heart.py
@@ -14,19 +14,19 @@ async def _rename(bot: Client, msg: Message):
     # New Name
     q = SESSION.query(Users).get(msg.from_user.id)
     if q.running:
-        await msg.reply("One at a Time !", quote=True)
+        await msg.reply("Tek seferde bir dosya gönderin !", quote=True)
         return
     q.running = True
     try:
         new_name_message = await bot.ask(
             msg.chat.id,
-            "What should be the new name ? \n\n[To cancel send `/cancel`]",
+            "Dosyanın yeni adını giriniz ! \n\n[İşlemi durdurmak için `/cancel` komutunu veriniz !]",
             filters=filters.user(msg.from_user.id) & filters.text,
         )
         # Cancel 1
         cancelled = await is_cancel(new_name_message)
         if cancelled:
-            await new_name_message.reply("Cancelled the Process !", quote=True)
+            await new_name_message.reply("İşlem durduruldu !", quote=True)
             return
         # Extension
         if msg.video:
@@ -40,17 +40,17 @@ async def _rename(bot: Client, msg: Message):
                 new_name = new_name + "." + extension
         surely_question = await bot.send_message(
             msg.chat.id,
-            f"Are you sure that '`{new_name}`' should be the new name ? \n\nIf yes, reply with 'y' or 'yes'. \nIf no, reply with 'n' or 'no'. New name will be asked again. \nTo cancel send `/cancel`"
+            f"Dosyayı '`{new_name}`' şeklinde adlandırmak istediğinize emin misiniz ? \n\nEğer yanıtınız evet ise, 'y' veya 'yes' ile, \ndeğilse, 'n' veya 'no' ile yanıtlayın.  \nIptal etmek için `/cancel` komutunu gönderin"
             f"\n\n[To prevent spelling/typing mistakes.]"
         )
         surely = await bot.listen(msg.chat.id, timeout=300, filters=filters.user(msg.from_user.id) & filters.text)
         # Cancel 2
         cancelled = await is_cancel(surely)
         if cancelled:
-            await new_name_message.reply("Cancelled the Process !", quote=True)
+            await new_name_message.reply("İşlem iptal edildi !", quote=True)
             return
         if surely.text.lower() in ["n", "no"]:
-            new_name = (await bot.ask(msg.chat.id, "\nWhat should be the new name ?\n")).text
+            new_name = (await bot.ask(msg.chat.id, "\nDosyanın yeni adını giriniz! \n")).text
             if "." in old_name:
                 extension = old_name.rsplit(".", 1)[1]
                 if "." not in new_name:
@@ -65,29 +65,29 @@ async def _rename(bot: Client, msg: Message):
         else:
             thumb = None
         # Downloading
-        downloading = await msg.reply("**Downloading...**")
-        file_path = await msg.download(progress=progress, progress_args=(downloading, "Downloading..."))
-        await downloading.edit("**Downloaded.**")
+        downloading = await msg.reply("**İndiriliyor...**")
+        file_path = await msg.download(progress=progress, progress_args=(downloading, "İndiriliyor..."))
+        await downloading.edit("**İndirildi.**")
         await downloading.delete()
 
         # Uploading
-        uploading = await downloading.reply("**Now Uploading...**")
+        uploading = await downloading.reply("**Yükleniyor...**")
         if msg.caption is None:
             caption = ""
         else:
             caption = msg.caption
         if q.video_to == "video":
             await bot.send_video(chat_id=msg.chat.id, video=file_path, file_name=new_name, caption=caption,
-                                 thumb=thumb, progress=progress, progress_args=(uploading, "Uploading..."))
+                                 thumb=thumb, progress=progress, progress_args=(uploading, "Yükleniyor..."))
         else:
             await bot.send_document(chat_id=msg.chat.id, document=file_path, file_name=new_name, caption=caption,
-                                    thumb=thumb, progress=progress, progress_args=(uploading, "Uploading..."))
+                                    thumb=thumb, progress=progress, progress_args=(uploading, "Yükleniyor..."))
         SESSION.close()
-        await uploading.edit("**Uploaded.**")
+        await uploading.edit("**Yüklendi!**")
         await uploading.delete()
     except Exception as e:
         await msg.reply(
-            f"Error : {e} \n\nTry Again and if still doesn't work then forward this message to @StarkBots !")
+            f"Error : {e} \n\nİşlem başarısız !")
     finally:
         q = SESSION.query(Users).get(msg.from_user.id)
         q.running = False
